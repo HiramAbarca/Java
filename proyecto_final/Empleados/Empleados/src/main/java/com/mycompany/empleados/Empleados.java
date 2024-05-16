509e39256bd1f22994c6f8fa448e241de153548c
HiramAbarca <151661923+HiramAbarca@users.noreply.github.com>
2024-05-16 11:05:42 -0600

    Add files via upload

---
+++ proyecto_final/Empleados/Empleados/src/main/java/com/mycompany/empleados/Empleados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.empleados;
+
+/**
+ *
+ * @author PILARES
+ */
+public class Empleados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
